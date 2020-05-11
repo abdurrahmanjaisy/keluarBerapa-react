ccd0545613ba08b45f1cf2375fbc900633b7cba4
royandi <royandinicolas@mail.ugm.ac.id>
2020-05-11 11:08:19 +0700

    Updates

---
+++ precache-manifest.4be297f631706e3b13f9d235a82e79dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e288214161d0388d3db983cbaf53e0",
+    "url": "/keluarBerapa-react/index.html"
+  },
+  {
+    "revision": "6c56f50d32943d943eaa",
+    "url": "/keluarBerapa-react/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "5fd0c2421f0dc454ba8f",
+    "url": "/keluarBerapa-react/static/css/main.59e576c0.chunk.css"
+  },
+  {
+    "revision": "6c56f50d32943d943eaa",
+    "url": "/keluarBerapa-react/static/js/2.1ce63dfa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/keluarBerapa-react/static/js/2.1ce63dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd0c2421f0dc454ba8f",
+    "url": "/keluarBerapa-react/static/js/main.b84b34ca.chunk.js"
+  },
+  {
+    "revision": "6c65aabbed38cbc2cb40",
+    "url": "/keluarBerapa-react/static/js/runtime-main.1a54df34.js"
+  }
+]);
